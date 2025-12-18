ceb2bd0afa127d09a96bbcf79861c153a4632a4b
Edmond <edebeid@gmail.com>
2025-12-18 08:32:17 -0500

    Enable debug mode for Auth authentication to diagnose server error

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -5,6 +5,7 @@ import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import prisma from "@/lib/prisma";
 
 export const authOptions = {
+    debug: true,
     adapter: PrismaAdapter(prisma),
     providers: [
         GoogleProvider({
