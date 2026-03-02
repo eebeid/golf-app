ae6b05a4a43f68b216e0dc7c86b3fc999ba5e81a
Edmond <edebeid@gmail.com>
2026-03-01 23:33:06 -0500

    Dynamically link version.js to package.json version

--- src/lib/version.js
+++ src/lib/version.js
@@ -1,2 +1,3 @@
-// Bump this any time you deploy a meaningful update
-export const APP_VERSION = '1.0.0';
+import packageJson from '../../package.json';
+
+export const APP_VERSION = packageJson.version;
